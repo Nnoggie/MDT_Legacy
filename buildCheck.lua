55ca82bea80ac51a94333b0912d2ea6696f18e51
Nnoggie <27819512+Nnoggie@users.noreply.github.com>
2026-07-19 05:04:28 +0200

    feat: archive MoP dungeons on Retail

--- buildCheck.lua
+++ buildCheck.lua
@@ -5,8 +5,3 @@ function addon:IsRetail()
   local gameVersion = select(4, GetBuildInfo())
   return gameVersion >= 110000
 end
-
-function addon:IsMop()
-  local gameVersion = select(4, GetBuildInfo())
-  return gameVersion >= 50000 and gameVersion < 60000
-end
